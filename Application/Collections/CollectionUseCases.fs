f25ae8eba7869295a7c580cd8da7cc10fb2ab1d2
Andrew Malikov <andrew.devels@gmail.com>
2021-12-23 15:21:28 +0300

    feat: add filter builders

--- Application/Collections/CollectionUseCases.fs
+++ Application/Collections/CollectionUseCases.fs
@@ -1,12 +1,15 @@
 namespace Application.Collections
 
+open FsToolkit.ErrorHandling
+
 open Domain.Collections.CollectionEntity
+open Domain.Collections.CollectionFilters
 
 module CollectionUseCases =
     let getMetadata fetchCollection mapCollection collectionId username : Async<CollectionMetadata> =
         async {
             let! collectionMetadata = fetchCollection collectionId username
-            
+
             return mapCollection collectionMetadata
         }
 
@@ -18,4 +21,18 @@ module CollectionUseCases =
             return
                 { metadata = metadata
                   projects = projects }
+        }
+        
+    let getCollectionFilters getProjectFilters filterOptions =
+        result {
+            let! projectFilters = getProjectFilters filterOptions
+            
+            return filterCollection projectFilters
+        }
+
+    let getFilteredCollection getCollection filterCollection collectionId username : Async<Collection option> =
+        async {
+            let! collection = getCollection collectionId username
+
+            return filterCollection collection
         }
--- Application/Projects/ProjectUseCases.fs
+++ Application/Projects/ProjectUseCases.fs
@@ -1,12 +1,25 @@
 namespace Application.Projects
 
+open FsToolkit.ErrorHandling
+
+open Domain.Assets.AssetFiltersBuilder
 open Domain.Projects.ProjectEntity
+open Domain.Projects.ProjectFilters
+open Domain.Projects.ProjectFiltersBuilder
 
 module ProjectUseCases =
     let getProjects fetchProjects mapProject collectionId : Async<Project list> =
         async {
             let! projects = fetchProjects collectionId
-            
+
             return projects |> List.map mapProject
         }
 
+    let getProjectFilters filterOptions =
+        result {
+            let! assetFilters = buildAssetFilters filterOptions
+
+            return
+                buildProjectFilters filterOptions
+                @ [ fun project -> filterProject assetFilters project |> Some ]
+        }
