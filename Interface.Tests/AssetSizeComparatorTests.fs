139116b481ba0253126f41cc6c99adc915dc7b95
Andrew Malikov <andrew.devels@gmail.com>
2022-02-19 13:35:46 +0300

    test: add more asset size comparator tests

--- Interface.Tests/AssetSizeComparatorTests.fs
+++ Interface.Tests/AssetSizeComparatorTests.fs
@@ -1,5 +1,6 @@
 namespace Interface.Tests
 
+open Microsoft.FSharp.Reflection
 open Xunit
 open FsUnit.Xunit
 open FsUnit.CustomMatchers
@@ -8,14 +9,22 @@ open Domain.Assets.AssetFilters
 open Interface.Assets.AssetArgs
 
 module AssetSizeComparatorTests =
-    [<Fact>]
-    let ``GIVEN valid greater query WHEN parseAssetSizeComparator THEN returns Ok Some Comparator`` () =
-        //  Arrange
-        let validGreaterQuery = "size>=1920:1080"
+    type TestData() =
+        static member ValidComparatorQueries =
+            [ ("size>=1920:1080", GreaterOrEqual { width = 1920; height = 1080 })
+              ("size=3280:2200", Equal { width = 3280; height = 2200 })
+              ("size<=400:400", LessOrEqual { width = 400; height = 400 })
+              ("size>0:0", Greater { width = 0; height = 0 })
+              ("size<2000:4000", Less { width = 2000; height = 4000 }) ]
+            |> Seq.map FSharpValue.GetTupleFields
 
+    [<Theory; MemberData("ValidComparatorQueries", MemberType = typeof<TestData>)>]
+    let ``GIVEN valid greater query WHEN parseAssetSizeComparator THEN returns Ok Some Comparator``
+        query
+        expectedComparator
+        =
         // Act
-        let actualComparator =
-            parseAssetSizeComparator validGreaterQuery
+        let actualComparator = parseAssetSizeComparator query
 
         // Assert
         match actualComparator with
@@ -23,6 +32,6 @@ module AssetSizeComparatorTests =
             match comparator with
             | Some assetSizeComparator ->
                 assetSizeComparator
-                |> should be (ofCase <@ GreaterOrEqual { width = 1920; height = 1080 } @>)
+                |> should be (equal expectedComparator)
             | _ -> failwith "Expected Some Comparator"
         | _ -> failwith "Expected Ok Some Comparator"
