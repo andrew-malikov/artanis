b0879d614128a0899adf1aee0fa166a193d18d56
Andrew Malikov <andrew.devels@gmail.com>
2022-02-19 13:55:53 +0300

    test: add invalid queries of asset size comparator tests

--- Interface.Tests/AssetSizeComparatorTests.fs
+++ Interface.Tests/AssetSizeComparatorTests.fs
@@ -1,5 +1,6 @@
 namespace Interface.Tests
 
+open System
 open Microsoft.FSharp.Reflection
 open Xunit
 open FsUnit.Xunit
@@ -18,8 +19,20 @@ module AssetSizeComparatorTests =
               ("size<2000:4000", Less { width = 2000; height = 4000 }) ]
             |> Seq.map FSharpValue.GetTupleFields
 
+        static member InvalidComparatorQueries =
+            [ "comparator>=1920:1080"
+              "size=-3280:2200"
+              "size<=400:-400"
+              "size>-0:-0"
+              "size==2000:4000"
+              "size=480.5:330"
+              "size<=445:-400.33"
+              ">0:0"
+              "size>>2035:00" ]
+            |> Seq.map (fun invalidQuery -> [ invalidQuery :> Object ] |> Seq.toArray)
+
     [<Theory; MemberData("ValidComparatorQueries", MemberType = typeof<TestData>)>]
-    let ``GIVEN valid greater query WHEN parseAssetSizeComparator THEN returns Ok Some Comparator``
+    let ``GIVEN valid comparator query WHEN parseAssetSizeComparator THEN returns Ok Some Comparator``
         query
         expectedComparator
         =
@@ -35,3 +48,13 @@ module AssetSizeComparatorTests =
                 |> should be (equal expectedComparator)
             | _ -> failwith "Expected Some Comparator"
         | _ -> failwith "Expected Ok Some Comparator"
+
+    [<Theory; MemberData("InvalidComparatorQueries", MemberType = typeof<TestData>)>]
+    let ``GIVEN invalid comparator query WHEN parseAssetSizeComparator THEN returns Error string`` invalidQuery =
+        // Act
+        let actualComparator = parseAssetSizeComparator invalidQuery
+
+        // Assert
+        match actualComparator with
+        | Error message -> message |> should not' EmptyString
+        | _ -> failwith "Expected Error string"
